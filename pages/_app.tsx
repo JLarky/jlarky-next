61b509368173b9eb63fb0d544193f331afda29c4
rumta123 <49255220+rumta123@users.noreply.github.com>
2020-05-01 13:20:37 +0300

    добавлено чуть чуть кода
    Router.events.on('routeChangeComplete', url => gtag.pageview(url))
    export default function App({ Component, pageProps }: AppProps) {
      return <Component {...pageProps} />

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import '../styles/tailwind.css'
 import { AppProps } from 'next/app'
-
+import * as gtag from '../lib/gtag'
+import { Router } from 'next/router'
+Router.events.on('routeChangeComplete', url => gtag.pageview(url))
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
