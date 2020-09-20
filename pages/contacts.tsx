4517f476893dbd714d20fa473b03551a33f42c8f
JLarky <jlarky@gmail.com>
2020-09-20 14:53:56 -0700

    update contacts

--- pages/contacts.tsx
+++ pages/contacts.tsx
@@ -16,9 +16,24 @@ export const Contacts: React.FC<Props> = ({ lastModified }) => {
         className="w-full px-4 md:px-6 text-xl text-gray-800 leading-normal"
         style={{ fontFamily: 'Georgia,serif' }}
       >
-        <p title={lastModified}>
+        <p className="py-2">Hi!</p>
+        <p className="py-2">
+          On most things on the internet I have nick <b>JLarky</b> or{' '}
+          <b>jlarky2012</b> (it was funnier before the end of the world
+          happened, I should have used jlarky2020). But my real name is Yaroslav
+          Ivanovich Lapin (Ярослав Иванович Лапин), I was born in USSR and lived
+          most of my live in Russia but last few years I live in USA. I rarely
+          care about updating this kind of information on social networks, so
+          this is going to go stale at some point as well :)
+        </p>
+        <p className="py-2">
+          I care about some people, games and programming. Lately I mostly code
+          in React/TypeScript and Elixir.
+        </p>
+        <p className="py-2" title={lastModified}>
           Right at this moment (<Date dateString={lastModified} />) I would say
-          that best way to reach me is Telegram or Twitter.
+          that best way to reach me is Telegram or Twitter. But I have some
+          other links that I'm going to add here in time :)
         </p>
       </section>
       <div className="py-6"> </div>
