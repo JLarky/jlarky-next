43243583a08b534177d4f6ad9210cc7096ac6a13
JLarky <jlarky@gmail.com>
2020-09-19 21:02:47 -0700

    update with with-tailwindcss example

--- postcss.config.js
+++ postcss.config.js
@@ -1,16 +1,18 @@
-const purgecss = [
-  '@fullhuman/postcss-purgecss',
-  {
-    content: ['./{components,lib,pages,posts,public}/**/*.{html,ts,tsx,md}'],
-    defaultExtractor: content => content.match(/[A-Za-z0-9_\-:./]+/g) || []
-  }
-]
-
 module.exports = {
   plugins: [
-    'postcss-import',
     'tailwindcss',
-    'autoprefixer',
-    ...(process.env.NODE_ENV === 'production' ? [purgecss] : [])
+    'postcss-flexbugs-fixes',
+    [
+      'postcss-preset-env',
+      {
+        autoprefixer: {
+          flexbox: 'no-2009'
+        },
+        stage: 3,
+        features: {
+          'custom-properties': false
+        }
+      }
+    ]
   ]
 }
